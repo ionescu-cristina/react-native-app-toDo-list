324bc9d7d4a091b7475f502758e3daf7ddc501e7
Ionescu Cristina <58083891+ionescu-cristina@users.noreply.github.com>
2022-11-01 12:16:44 -0700

    add ScrollView

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StatusBar} from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
+import React ,  {useState} from 'react';
 
 export default function App() {
+  const [enteredTasks, setEnteredTasks] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const taskInputHandler = (enteredText) => {addTaskHandler
+    setEnteredTasks(enteredText);
+  }
+
+  const addTaskHandler = () => {
+    setTasks(currentTask => [...currentTask, enteredTasks])
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Add new words"
           style={styles.input}
+          onChangeText= {taskInputHandler}
+          value = {enteredTasks}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addTaskHandler} />
       </View>
+      <ScrollView>{ tasks.map((task) =><View key="{task}"   style={styles.listItem}><Text >{task}</Text></View>)  }</ScrollView>
     </View>
   );
 }
@@ -29,5 +43,11 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
+  },
+  listItem: {
+    padding:10,
+    borderColor:'black',
+    borderWidth: 1,
+    marginVertical:5
   }
 });
